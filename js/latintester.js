7989dd773e7fbd44bd9e7e2e166797beca674143
mrlaurence <mrlaurence@users.noreply.github.com>
2016-07-21 16:06:48 +0100

    v3.0 - Overhauled the interface again, added a navigation bar and the About page

--- js/latintester.js
+++ js/latintester.js
@@ -1,13 +1,13 @@
 var latinTester = (function() {
     'use strict';
-    
+
     //Functions here are accessible from inside the module only
-    
+
     //General-purpose variables accessible from everywhere within this function
     var vocabDictionary = {},
         vocabKeyArray,
         currentKeyArrayIndex;
-    
+
     //Variables pointing to HTML elements accessible from everywhere this function
     var startTestButton,
         testOptionsContainer,
@@ -19,39 +19,38 @@ var latinTester = (function() {
         submitButton,
         correctStatusText,
         continueButton;
-    
-    //Defines variables that point to HTML elements
+
     function setupElements() {
-        startTestButton = document.getElementById("start_button");
-        testOptionsContainer = document.getElementById("test_options_container");
-        aosSelect = document.getElementById("aosSelect");
-        tiersSelect = document.getElementById("tiersSelect");
-        testContainer = document.getElementById("test_container");
-        testWord = document.getElementById("test_word");
-        translationInput = document.getElementById("translation_input");
-        submitButton = document.getElementById("submit_button");
-        continueButton = document.getElementById("continue_button");
-        correctStatusText = document.getElementById("correct_status");
+        startTestButton = document.getElementById("start-button");
+        testOptionsContainer = document.getElementById("test-options-container");
+        aosSelect = document.getElementById("aos-select");
+        tiersSelect = document.getElementById("tiers-select");
+        testContainer = document.getElementById("test-container");
+        testWord = document.getElementById("test-word");
+        translationInput = document.getElementById("translation-input");
+        submitButton = document.getElementById("submit-button");
+        continueButton = document.getElementById("continue-button");
+        correctStatusText = document.getElementById("correct-status");
     }
-    
+
     //Parses the text from the vocabulary text file by creating a vocab dictionary
     function parseVocabFileText(vocabFileText) {
-        
+
         /*
         The text that is imported directly from the vocabulary file follows the following structure:
-        
+
         Latin Word   How the word declines/conjugates   English translation   Included in Higher Tier only? (Y/N)   Included in AO41? (Y/N)   Included in AO42? (Y/N)
         Latin Word 2   How the word declines/conjugates   English translation   Included in Higher Tier only? (Y/N)   Included in AO41? (Y/N)   Included in AO42? (Y/N)
-        
+
         Each word with its corresponding values is on a seperate line and each of the values is seperated by a tab. The tabs cause 'columns' within the text to be formed.
-        
+
         Two examples of a line of the file:
         alius   alia, aliud   other, another, else   Y   Y   Y
         bellum gero   gerere, gessi, gestus   wage war   N   N   Y
-        
+
 
         The vocab dictionary follows the following structure:
-        
+
         {"latin_word_1": ["english_translation_1", "english_translation_2", "etc."],
          "latin_word_2": ["english_translation_1", "english_translation_2", "etc."]
         }
@@ -61,47 +60,47 @@ var latinTester = (function() {
          "aqua": ["water"]
         }
         */
-        
+
         //The script will now check which tiers and AOs will be imported to the vocab dictionary
         var importao41,
             importao42,
             importft,
             importht;
-        
+
         switch(aosSelect.options[aosSelect.selectedIndex].value) {
             case "Both AOs":
                 importao41 = true;
                 importao42 = true;
                 break;
-                
+
             case "AO41 only":
                 importao41 = true;
                 importao42 = false;
                 break;
-                
+
             case "AO42 only":
                 importao41 = false;
                 importao42 = true;
                 break;
         }
-        
+
         switch(tiersSelect.options[tiersSelect.selectedIndex].value) {
             case "Both Tiers":
                 importft = true;
                 importht = true;
                 break;
-                
+
             case "Foundation Tier only":
                 importft = true;
                 importht = false;
                 break;
-                
+
             case "Higher Tier only":
                 importft = false;
                 importht = true;
-                break; 
+                break;
         }
-        
+
         //The script will now parse the vocab file
         var tabCount = 0,
             latinWord = "",
@@ -110,17 +109,17 @@ var latinTester = (function() {
             currentChar,
             willImportBasedOnTier = false,
             willImportBasedOnAO = false;
-        
+
         for (var i = 0; i <= vocabFileText.length -1; i+= 1) {
             currentChar = vocabFileText.charAt(i);
-            
+
             if (currentChar === '\n' || vocabFileText.length === i + 1) {
                 //The script has reached a new line character or the last character in the text so the script needs to add the result of parsing the line to the vocab dictionary, if appropriate according to the tier(s) and AO(s) selected by the user
-                
+
                 if (willImportBasedOnTier && willImportBasedOnAO) {
                     vocabDictionary[latinWord] = englishTranslations;
                 }
-                
+
                 //Set all the variables used while parsing the line back to their original values so the script can use them for the next line
                 tabCount = 0;
                 latinWord = "";
@@ -144,7 +143,7 @@ var latinTester = (function() {
                     //The vocabulary tester does not use the second column, which contains the way the Latin word declines/conjugates
                     case 2:
                         //The third column contains the English translation
-                        
+
                         if (currentChar === ',') {
                             englishTranslations.push(currentEnglishTranslation.trim());
                             currentEnglishTranslation = "";
@@ -185,57 +184,57 @@ var latinTester = (function() {
                 }
             }
         }
-        
+
         //After the script finishes parsing the text from the vocab file, the script adds the keys (which are the Latin versions of the words) from the vocab dictionary to an array so it can use them later
         vocabKeyArray = Object.keys(vocabDictionary);
     }
-    
+
     //This function is run when the user submits their attempted translation. It's purpose is to modify a certain text element to inform the user as to whether their attempted translation was correct or incorrect
     function setCorrectStatus(isCorrect) {
         var statusInnerHtml,
             statusColour;
-        
+
         function changeCorrectStatusTextElement(innerHtml, colour) {
             correctStatusText.innerHTML = statusInnerHtml;
             correctStatusText.style.color = statusColour;
             correctStatusText.style.display = "block";
         }
-        
+
         if (isCorrect) {
-            
+
             //The script informs the user they were correct and continues to test them on another word
             statusInnerHtml = "Correct";
-            statusColour = "#33C3F0";
+            statusColour = "#18B495";
             changeCorrectStatusTextElement(statusInnerHtml, statusColour);
-            
+
             latinTester.changeWord();
-            
+
             //After a short delay, hide the element with the text that informs the user as to whether they are correct or not
             setTimeout(function () {
                 correctStatusText.style.display = "none";
             }, 300);
         }
         else {
-            
+
             //The script informs the user they were incorrect and does not continue to test them on another word until they click the continue button. Instead, it displays the correct translations(s)
-            
+
             //Get an array of correct translations
             var correctTranslations = vocabDictionary[vocabKeyArray[currentKeyArrayIndex]];
-            
+
             //Compile a string of all the correct translations from the array seperated by commas
             var correctTranslationsString = "";
-            
+
             for (var i = 0; i < correctTranslations.length; i += 1) {
                 correctTranslationsString += correctTranslations[i];
-                
+
                 if (i + 1 != correctTranslations.length) {
                     correctTranslationsString += ", ";
                 }
             }
             statusInnerHtml = "Incorrect - " + correctTranslationsString;
-            statusColour = "#ff704d";
+            statusColour = "#E74C3C";
             changeCorrectStatusTextElement(statusColour, statusColour);
-            
+
             //Replace the translation input field and the submit button
             submitButton.style.display = "none";
             translationInput.style.display = "none";
@@ -243,80 +242,80 @@ var latinTester = (function() {
             continueButton.focus();
         }
     }
-    
+
     return {
         //Functions here are accessible inside and outside the module
-        
+
         //Run when the user starts the test. The purpose of this function is to setup the test by hiding the test options, get the text from the vocabulary file, call the function to parse the vocabulary file text, etc.
         startTest: function() {
             setupElements();
-            
+
             //Don't display elements that are no longer needed
             testOptionsContainer.style.display = "none";
             startTestButton.style.display = "none";
-            
+
             //Display the actual test interface
             testContainer.style.display = "block";
-            
+
             //Get the text from the vocabulary file
             var xhr = new XMLHttpRequest();
-            
+
             function handleStateChange() {
                 if (xhr.readyState === 4) {
-                    
+
                     //Once the script has the text, parse it
                     parseVocabFileText(xhr.responseText);
-                    
+
                     //Now that the script has compiled the vocab dictionary, it can start testing by displaying a Latin word to the user
                     latinTester.changeWord();
                 }
             }
             xhr.onreadystatechange = handleStateChange;
-            xhr.open("GET", "/ocrlatintester/assets/vocabulary_data.txt", true);
+            xhr.open("GET", "/txt/vocabulary-data.txt", true);
             xhr.send();
         },
-        
+
         //Change the Latin word the user is being tested on
         changeWord: function() {
             currentKeyArrayIndex = Math.floor(Math.random() * (vocabKeyArray.length + 1));
             var randomWord = vocabKeyArray[currentKeyArrayIndex];
-            
+
             testWord.innerHTML = randomWord;
-            
+
             //Blank the input box for the translation and return the focus to it
             translationInput.value = "";
             translationInput.focus();
-            
+
         },
-        
+
         //Determine whether the user's attempted translation is correct
         validateTranslationAttempt: function() {
             var userTranslation = translationInput.value.toLowerCase(),
-                
+
             //Get the correct translation(s) for the Latin word the user is being tested on
             correctTranslations = vocabDictionary[vocabKeyArray[currentKeyArrayIndex]],
-                
+
             lowerCorrectTranslations = [];
-            
+
             for (var i = 0; i < correctTranslations.length; i += 1) {
                 lowerCorrectTranslations[i] = correctTranslations[i].toLowerCase();
             }
-            
+
             //Check whether the user's translation is in the array of valid translation(s) for the Latin word the user is being tested on
             if (lowerCorrectTranslations.indexOf(userTranslation) > -1) {
                 //It is in the array - the user was correct
-                
+
                 //Inform them they were correct
                 setCorrectStatus(true);
             }
             else {
                 //It is not in the array - the user was incorrect
-                
+
                 //Inform them they were incorrect and of the correct translation(s) for the Latin word
                 setCorrectStatus(false);
             }
         },
-        
+
         //Runs after the user presses the continue button after they have translated a word incorrectly and are being prompted with the correct translation(s). This function resumes the test by prompting the user with another word to translate as well as removing the correct status text and continue button
         continueTest: function() {
             continueButton.style.display = "none";
@@ -326,22 +325,15 @@ var latinTester = (function() {
             correctStatusText.style.display = "none";
             latinTester.changeWord();
         },
-        
+
         //Runs when the user presses a key when certain elements are in focus. The purpose of this function is to simulate a click on the element that was in focus if it was the Return key that was pressed
         keyPress: function(event, source) {
-            
+
             if (event.keyCode === 13) {
                 //The Return key was pressed
-                
                 switch (source) {
-                    case "translation_input":
+                    case "translation-input":
                         submitButton.click();
-                        break;
-                    case "continue_button":
-                        continueButton.click();
-                        break;
-                    case "start_test":
-                        startTestButton.click();
                         break;
                 }
             }
